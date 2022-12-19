61f23d02609e2992a9351a33f74611fb1dbf03bb
shilpadey <shilpadey735@gmail.com>
2022-12-19 18:01:55 +0530

    authcontextprovider in app

--- src/App.js
+++ src/App.js
@@ -4,19 +4,20 @@ import './App.css';
 import AuthForm from './component/Auth/AuthForm';
 import HomePage from './pages/HomePage';
 import AuthContext from './store/auth-context';
+import { AuthContextProvider } from './store/auth-context';
 
 
 function App() {
   const athCntx = useContext(AuthContext);
   console.log(athCntx);
   return (
-      <React.Fragment>
+      <AuthContextProvider>
         <Routes>
           {!athCntx.isLoggedIn && <Route path='/auth' element={ <AuthForm /> }/>}
           {athCntx.isLoggedIn && <Route path='/home'  element={ <HomePage /> }/>}
           <Route path="*" element={<Navigate replace to='/auth' />} />
         </Routes>
-      </React.Fragment>
+      </AuthContextProvider>
   );
 }
 
